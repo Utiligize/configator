9b07dd4b0f1f7a035dfc61b66b4e4971fe950a56
Kristian Thy <kthy@utiligize.com>
2025-11-15 02:27:28 +0100

    fix: export symbols

--- src/configator/__init__.py
+++ src/configator/__init__.py
@@ -1,8 +1,16 @@
 from importlib.metadata import version
 
 from configator.core import load_config
+from configator.models import Environment, PostgresConfig, SentryConfig
 
 __maintainer__ = "kthy"
 __version__ = version("configator")
 
-__all__ = ["__maintainer__", "__version__", "load_config"]
+__all__ = [
+    "Environment",
+    "PostgresConfig",
+    "SentryConfig",
+    "__maintainer__",
+    "__version__",
+    "load_config",
+]
